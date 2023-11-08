84ac699ae14dbfebe1e570b8d5291ce2ddfa2766
jabrock <john.brock@oracle.com>
2023-11-07 16:49:38 -0800

    adding chart with drill example

--- src/components/content/examples/dataviz/index.tsx
+++ src/components/content/examples/dataviz/index.tsx
@@ -1,4 +1,5 @@
 import Chart from "./chart";
+import { DrillChart } from "./chart-drill";
 import Legend from "./legend";
 import { h } from "preact";
 
@@ -13,6 +14,10 @@ const DataViz = () => {
         <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
           <h2 class="oj-typography-heading-sm"> Standalone Legend </h2>
           <Legend />
+        </div>
+        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <h2 class="oj-typography-heading-sm"> Drillable Chart </h2>
+          <DrillChart />
         </div>
       </div>
     </div>
