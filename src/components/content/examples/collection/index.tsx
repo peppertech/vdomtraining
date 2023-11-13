dfd27c6e4b2b0c50cc40708fc2228a8b040956f4
jabrock <john.brock@oracle.com>
2023-11-13 11:00:34 -0800

    updating panel width css and code.

--- src/components/content/examples/collection/index.tsx
+++ src/components/content/examples/collection/index.tsx
@@ -8,19 +8,19 @@ const Collection = () => {
   return (
     <div class="oj-web-applayout-max-width oj-web-applayout-content">
       <div class="oj-flex oj-sm-flex-items-1">
-        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-collection-panel">
+        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-lg">
           <h2 class="oj-typography-heading-sm"> Table </h2>
           <Table />
         </div>
-        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-collection-panel">
+        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-lg">
           <h2 class="oj-typography-heading-sm"> ListView </h2>
           <ListView />
         </div>
-        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-collection-panel">
+        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-md">
           <h2 class="oj-typography-heading-sm"> Treeview </h2>
           <Treeview />
         </div>
-        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-collection-panel">
+        <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-lg">
           <h2 class="oj-typography-heading-sm"> DataGrid </h2>
           <DataGrid />
         </div>
--- src/components/content/examples/navlayout/index.tsx
+++ src/components/content/examples/navlayout/index.tsx
@@ -23,45 +23,48 @@ const NavLayout = () => {
         onojAction={toggleDrawer}
         aria-label="To go to drawer content, locate header with name Drawer Content"
         label="Toggle End Drawer"
-        chroming="callToAction"></oj-button>
+        chroming="callToAction"
+      ></oj-button>
       <oj-drawer-layout endOpened={opened} class="demo-full-height">
         <div class="oj-flex oj-sm-flex-items-1">
-          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-md">
             <h2 class="oj-typography-heading-sm"> Navigation List </h2>
             <NavList />
           </div>
-          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-md">
             <h2 class="oj-typography-heading-sm">
               {"Accordion and Collapsible"}
             </h2>
             <Accordion />
           </div>
-          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-md">
             <h2 class="oj-typography-heading-sm"> Action Card </h2>
             <ActionCard />
           </div>
-          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-sm">
             <h2 class="oj-typography-heading-sm"> Tabbar </h2>
             <TabBar />
           </div>
-          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-sm">
             <h2 class="oj-typography-heading-sm"> Dialog </h2>
             <Dialog />
           </div>
-          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel">
+          <div class="oj-flex-item oj-panel oj-sm-margin-2x demo-panel-sm">
             <h2 class="oj-typography-heading-sm"> Popup </h2>
             <Popup />
           </div>
         </div>
         <div
           slot="end"
-          class="oj-color-invert nav-drawer-light-bg demo-full-height">
+          class="oj-color-invert nav-drawer-light-bg demo-full-height"
+        >
           <div class="demo-drawer-header">
             <h6>Drawer Content</h6>
             <oj-button
               display="icons"
               chroming="borderless"
-              onojAction={toggleDrawer}>
+              onojAction={toggleDrawer}
+            >
               <span slot="startIcon" class="oj-ux-ico-close"></span>
               Close
             </oj-button>
