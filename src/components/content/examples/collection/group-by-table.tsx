1315d933f64eb09cc3a247cec23912c28ede2a82
jabrock <john.brock@oracle.com>
2024-03-04 12:38:02 -0800

    updated missing type info

--- src/components/content/examples/collection/group-by-table.tsx
+++ src/components/content/examples/collection/group-by-table.tsx
@@ -1,11 +1,12 @@
 import { ComponentProps } from "preact";
 import "ojs/ojtable";
-import {ojTable} from "ojs/ojtable";
+import { ojTable } from "ojs/ojtable";
+import { Item } from "ojs/ojdataprovider";
 import "ojs/ojrowexpander";
 import * as groupData from "text!./data/groupdata.json";
-import {MutableArrayTreeDataProvider} from "ojs/ojmutablearraytreedataprovider";
-import FlattenedTreeDataProviderView = require("ojs/ojflattenedtreedataproviderview")
-import {KeySetImpl} from 'ojs/ojkeyset';
+import { MutableArrayTreeDataProvider } from "ojs/ojmutablearraytreedataprovider";
+import FlattenedTreeDataProviderView = require("ojs/ojflattenedtreedataproviderview");
+import { KeySetImpl } from "ojs/ojkeyset";
 
 type Dept = {
   probability?: string;
@@ -23,7 +24,6 @@ type Dept = {
 type TableProps = ComponentProps<"oj-table">;
 
 const dataArray = JSON.parse(groupData);
-
 
 const setSelectionMode: TableProps["selectionMode"] = {
   row: "multiple",
@@ -72,7 +72,9 @@ const columnsDef: TableProps["columns"] = [
 ];
 
 const dataprovider: MutableArrayTreeDataProvider<Dept["id"], Dept> =
-  new MutableArrayTreeDataProvider(dataArray, "id", { keyAttributeScope: "global" });
+  new MutableArrayTreeDataProvider(dataArray, "id", {
+    keyAttributeScope: "global",
+  });
 
 const expanded = new KeySetImpl(["3"]);
 const flattenedTreeDataProviderView = new FlattenedTreeDataProviderView(
@@ -81,15 +83,15 @@ const flattenedTreeDataProviderView = new FlattenedTreeDataProviderView(
     expanded: expanded,
   }
 );
-// TODO Find out proper Type for item
-const setRowSelectable = (item:any) => {
+
+const setRowSelectable = (item: Item<Dept["id"], Dept>) => {
   if (item.metadata.treeDepth === 0) {
     return "off";
   }
   return "on";
 };
 
-const setRowSticky = (item:any) => {
+const setRowSticky = (item: Item<Dept["id"], Dept>) => {
   if (item.metadata.treeDepth === 0) {
     return "on";
   }
@@ -101,80 +103,73 @@ const setRowOptions: TableProps["row"] = {
   sticky: setRowSticky,
 };
 
-const rowItemTemplate = (row:ojTable.RowTemplateContext<Dept["id"], Dept>) => {
-return (
-  <tr>
-  {(row.item.metadata.treeDepth === 0) && (
-    <td colSpan={6} aria-label={row.item.data.accText} class="oj-sm-padding-0-start">
-      <div class="oj-flex-bar oj-sm-align-items-center">
-        <div class="oj-sm-padding-2x-horizontal">
-          <oj-row-expander context={row} data-oj-clickthrough="disabled"></oj-row-expander>
-        </div>
-        <div tabIndex={0} class="oj-typography-subheading-xs">
-          {row.item.data.salesstage}
-        </div>
-        <div class="oj-flex-bar-end oj-sm-text-align-end">
-          <div tabIndex={0}>
-            <span class="oj-typography-body-xs oj-text-color-secondary oj-typography-semi-bold">
-              Results
-            </span>
-            <div class="oj-sm-margin-0 oj-typography-body-md oj-typography-semi-bold">
-              {row.item.data.count}
+const rowItemTemplate = (row: ojTable.RowTemplateContext<Dept["id"], Dept>) => {
+  return (
+    <tr>
+      {row.item.metadata.treeDepth === 0 && (
+        <td
+          colSpan={6}
+          aria-label={row.item.data.accText}
+          class="oj-sm-padding-0-start"
+        >
+          <div class="oj-flex-bar oj-sm-align-items-center">
+            <div class="oj-sm-padding-2x-horizontal">
+              <oj-row-expander
+                context={row}
+                data-oj-clickthrough="disabled"
+              ></oj-row-expander>
+            </div>
+            <div tabIndex={0} class="oj-typography-subheading-xs">
+              {row.item.data.salesstage}
+            </div>
+            <div class="oj-flex-bar-end oj-sm-text-align-end">
+              <div tabIndex={0}>
+                <span class="oj-typography-body-xs oj-text-color-secondary oj-typography-semi-bold">
+                  Results
+                </span>
+                <div class="oj-sm-margin-0 oj-typography-body-md oj-typography-semi-bold">
+                  {row.item.data.count}
+                </div>
+              </div>
+              <div tabIndex={0} class="oj-sm-padding-10x-start">
+                <span class="oj-typography-body-xs oj-text-color-secondary oj-typography-semi-bold">
+                  Amount
+                </span>
+                <div class="oj-sm-margin-0 oj-typography-body-md oj-typography-semi-bold">
+                  {row.item.data.amount}
+                </div>
+              </div>
             </div>
           </div>
-          <div tabIndex={0} class="oj-sm-padding-10x-start">
-            <span class="oj-typography-body-xs oj-text-color-secondary oj-typography-semi-bold">
-              Amount
-            </span>
-            <div class="oj-sm-margin-0 oj-typography-body-md oj-typography-semi-bold">
-              {row.item.data.amount}
-            </div>
-          </div>
-        </div>
-      </div>
-    </td>
-  )}
-  {(row.item.metadata.treeDepth === 1) && (
-    <>
-    <td>
-      <span>
-        {row.item.data.probability}
-      </span>
-    </td>
-    <td>
-      <span>
-        {row.item.data.description}
-      </span>
-    </td>
-    <td>
-      <span>
-        {row.item.data.account}
-      </span>
-    </td>
-    <td>
-      <span>
-        {row.item.data.salesstage}
-      </span>
-    </td>
-    <td>
-      <span>
-        {row.item.data.amount}
-      </span>
-    </td>
-    <td>
-      <span>
-        {row.item.data.paymentdate}
-      </span>
-    </td>
-    </>
-  )}
-</tr>
-)
-}
-
+        </td>
+      )}
+      {row.item.metadata.treeDepth === 1 && (
+        <>
+          <td>
+            <span>{row.item.data.probability}</span>
+          </td>
+          <td>
+            <span>{row.item.data.description}</span>
+          </td>
+          <td>
+            <span>{row.item.data.account}</span>
+          </td>
+          <td>
+            <span>{row.item.data.salesstage}</span>
+          </td>
+          <td>
+            <span>{row.item.data.amount}</span>
+          </td>
+          <td>
+            <span>{row.item.data.paymentdate}</span>
+          </td>
+        </>
+      )}
+    </tr>
+  );
+};
 
 const GroupByTable = () => {
-
   return (
     <oj-table
       id="table"
@@ -187,10 +182,7 @@ const GroupByTable = () => {
       row={setRowOptions}
       columns={columnsDef}
     >
-      <template
-        slot="rowTemplate"
-        render={rowItemTemplate}
-      ></template>
+      <template slot="rowTemplate" render={rowItemTemplate}></template>
     </oj-table>
   );
 };
