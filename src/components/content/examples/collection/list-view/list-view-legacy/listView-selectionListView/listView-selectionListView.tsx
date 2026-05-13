3256c934a5eb3a4f436c030de1557cfd1cea3887
kaushaldeo21 <kaushal.deo21@hotmail.com>
2026-05-13 15:20:12 +0530

    card view selection issue fix

--- src/components/content/examples/collection/list-view/list-view-legacy/listView-selectionListView/listView-selectionListView.tsx
+++ src/components/content/examples/collection/list-view/list-view-legacy/listView-selectionListView/listView-selectionListView.tsx
@@ -52,43 +52,43 @@ export const ListViewSelectionListView = () => {
           id: 1,
           name: 'Chris Black',
           title: 'Oracle Cloud Infrastructure GTM Channel Director EMEA',
-          image: '../images/hcm/placeholder-male-01.png'
+          image: '/styles/images/hcm/placeholder-male-01.png'
       },
       {
           id: 2,
           name: 'Christine Cooper',
           title: 'Senior Principal Escalation Manager',
-          image: '../images/hcm/placeholder-female-01.png'
+          image: '/styles/images/hcm/placeholder-female-01.png'
       },
       {
           id: 3,
           name: 'Chris Benalamore',
           title: 'Area Business Operations Director EMEA & JAPAC',
-          image: '../images/hcm/placeholder-male-03.png'
+          image: '/styles/images/hcm/placeholder-male-03.png'
       },
       {
           id: 4,
           name: 'Christopher Johnson',
           title: 'Vice-President HCM Application Development',
-          image: '../images/hcm/placeholder-male-04.png'
+          image: '/styles/images/hcm/placeholder-male-04.png'
       },
       {
           id: 5,
           name: 'Samire Christian',
           title: 'Consulting Project Technical Manager',
-          image: '../images/hcm/placeholder-male-05.png'
+          image: '/styles/images/hcm/placeholder-male-05.png'
       },
       {
           id: 6,
           name: 'Kurt Marchris',
           title: 'Customer Service Analyst',
-          image: '../images/hcm/placeholder-male-06.png'
+          image: '/styles/images/hcm/placeholder-male-06.png'
       },
       {
           id: 7,
           name: 'Zelda Christian Cooperman',
           title: 'Senior Principal Escalation Manager',
-          image: '../images/hcm/placeholder-female-02.png'
+          image: '/styles/images/hcm/placeholder-female-02.png'
       }
   ], []);
   const dataProvider = useMemo(() => new ArrayDataProvider<EmployeeData['id'], EmployeeData>(data, {
