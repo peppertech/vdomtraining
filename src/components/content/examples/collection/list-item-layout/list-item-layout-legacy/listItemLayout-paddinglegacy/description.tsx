6987a84f9e71585a9b260a79ba0d2c1fbd366817
kaushaldeo21 <kaushal.deo21@hotmail.com>
2026-07-17 17:59:48 +0530

    more cleanup from collections

--- src/components/content/examples/collection/list-item-layout/list-item-layout-legacy/listItemLayout-paddinglegacy/description.tsx
+++
@@ -1,7 +0,0 @@
-import 'preact';
-
-const descriptionHtmlText = String.raw`<p>A List Item Layout represents layout used for list view item elements.</p>The demo shows how to render layout content using using a list view layout component with no outer padding.`;
-
-export const listItemLayoutPaddinglegacyDescription = (
-  <div dangerouslySetInnerHTML={{ __html: descriptionHtmlText }} />
-);
--- src/components/content/examples/collection/table/table-legacy/table-navigateTable/description.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import 'preact';
-
-export const tableNavigateTableDescription = (
-  <>
-    <p>A table displays data items in a tabular format with highly interactive features.</p>
-    <p>This demo shows how to save the scroll position, current row and selection state while navigating away from the table and restore them when the user navigates back.</p>
-  </>
-);
