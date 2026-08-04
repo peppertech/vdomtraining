3c0a1b68605722aae3ba8b6a8b2d51cd4d2e685f
kaushaldeo21 <kaushal.deo21@hotmail.com>
2026-08-04 11:36:26 +0530

    input password changes

--- src/components/content/examples/form/inputpassword/inputPasswordCorePack/index.tsx
+++ src/components/content/examples/form/inputpassword/inputPasswordCorePack/index.tsx
@@ -1,5 +1,6 @@
 import * as preact from 'preact';
 import { RecipePageTemplate } from "../../../../../shared/demo-page-layout/recipe-page-template";
+import type { PlaygroundConfig } from "../../../../../shared/code-playground/tsx-playground";
 import InputPasswordCorePackClearIconExample from "./inputPasswordCorePack-clearIcon";
 import {
   inputPasswordCorePackDocs,
@@ -7,6 +8,13 @@ import {
 } from "./inputPasswordCorePack-docs";
 import InputPasswordCorePackStatesExample from "./inputPasswordCorePack-states";
 import InputPasswordCorePackWidthExample from "./inputPasswordCorePack-width";
+import inputPasswordCorePackClearIconPlaygroundSource from "./inputPasswordCorePack-clearIcon-source";
+import inputPasswordCorePackStatesPlaygroundSource from "./inputPasswordCorePack-states-source";
+import inputPasswordCorePackWidthPlaygroundSource from "./inputPasswordCorePack-width-source";
+import {
+  labelEdgeOptions,
+  messageSets,
+} from "./inputPasswordCorePack-shared";
 
 const inputPasswordCorePackItems: {
   id: InputPasswordCorePackDemoId;
@@ -14,6 +22,7 @@ const inputPasswordCorePackItems: {
   description: (typeof inputPasswordCorePackDocs)[InputPasswordCorePackDemoId]["description"];
   recipe: (typeof inputPasswordCorePackDocs)[InputPasswordCorePackDemoId]["recipe"];
   Component: () => preact.JSX.Element;
+  playground?: PlaygroundConfig;
 }[] = [
   {
     id: "states",
@@ -21,6 +30,11 @@ const inputPasswordCorePackItems: {
     description: inputPasswordCorePackDocs.states.description,
     recipe: inputPasswordCorePackDocs.states.recipe,
     Component: InputPasswordCorePackStatesExample,
+    playground: {
+      initialSource: inputPasswordCorePackStatesPlaygroundSource,
+      fileName: "inputPasswordCorePack-states.tsx",
+      runtimeBindings: { messageSets },
+    },
   },
   {
     id: "clear-icon",
@@ -28,6 +42,10 @@ const inputPasswordCorePackItems: {
     description: inputPasswordCorePackDocs["clear-icon"].description,
     recipe: inputPasswordCorePackDocs["clear-icon"].recipe,
     Component: InputPasswordCorePackClearIconExample,
+    playground: {
+      initialSource: inputPasswordCorePackClearIconPlaygroundSource,
+      fileName: "inputPasswordCorePack-clearIcon.tsx",
+    },
   },
   {
     id: "width",
@@ -35,6 +53,11 @@ const inputPasswordCorePackItems: {
     description: inputPasswordCorePackDocs.width.description,
     recipe: inputPasswordCorePackDocs.width.recipe,
     Component: InputPasswordCorePackWidthExample,
+    playground: {
+      initialSource: inputPasswordCorePackWidthPlaygroundSource,
+      fileName: "inputPasswordCorePack-width.tsx",
+      runtimeBindings: { labelEdgeOptions },
+    },
   },
 ];
 
